889287f0a6659ff6bcf6cf363189b24aba5607eb
Chaehwi Hwang <chaehwi.h@gmail.com>
2023-10-20 16:46:11 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.java
@@ -3,8 +3,8 @@ class Solution {
        int[] answer = new int[num_list.length];
         
     
-        for(int i = num_list.length - 1, j = 0; i >= 0; i--, j++){
-            answer[j] = num_list[i];
+        for(int i = 0; i < num_list.length; i++){
+            answer[i] = num_list[num_list.length - 1 - i];
         }
         
         return answer;
