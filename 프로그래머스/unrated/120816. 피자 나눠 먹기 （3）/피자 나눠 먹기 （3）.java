efef5a35b1711cdfcf17e23a6dc477240275e506
Chaehwi Hwang <chaehwi.h@gmail.com>
2023-10-22 23:16:23 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -2,6 +2,6 @@ class Solution {
     public int solution(int slice, int n) {
         
         return n % slice > 0 ? n / slice + 1 :
-               n % slice == 0 ? n / slice : 0;
+               n / slice;
     }
 }
